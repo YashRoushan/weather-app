2aec92a75582bc37baa86d05111e66610056b110
yash roushan <yashroushan@gmail.com>
2024-08-22 00:13:38 -0300

    setting up the APIs and implementing the search city functionality

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '9f94111dadmshbad1661d73af8d0p1509aajsn5e39c2f1950f',
+        'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
